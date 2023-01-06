a7acf2468dd11ef5b4ed6f2d85d668e702b52c04
ejr <64696169+ericjrich@users.noreply.github.com>
2023-01-06 10:10:40 -0500

    Add files via upload

--- cnc/js.js
+++ cnc/js.js
@@ -6,52 +6,29 @@ $('#ejr').hide();
 	$('#ejr').fadeIn(500).delay(2000).fadeOut(500);
     $('#titlediv').delay(3000).fadeIn(500);
 	$('#pagediv').delay(3000).fadeIn(500);
-//===================================
-//===================================
-//https://htmlpreview.github.io/?https://github.com/ericjrich/sites/raw/tree/main/cnc/index.html
-//https://github.com/ericjrich/sites/raw/blob/main/radio/index.html
-$('#b00').click(function(){
-    $('#mainframe').attr('src', 'https://htmlpreview.github.io/?https://github.com/ericjrich/sites/raw/tree/main/cnc/b00/index.html');
-});
 
-$("#b00").mousedown(function(ev){
-      if(ev.which == 3)
-      {window.open('https://htmlpreview.github.io/?https://github.com/ericjrich/sites/raw/tree/main/cnc/b00/index.html', '_blank');}});
-
-//===================================
-$('#b01').click(function(){
-    $('#mainframe').attr('src', '.https://htmlpreview.github.io/?https://github.com/ericjrich/sites/raw/tree/main/radio/index.html');
-});
-
-$("#b01").mousedown(function(ev){
-      if(ev.which == 3)
-      {window.open('.https://htmlpreview.github.io/?https://github.com/ericjrich/sites/raw/tree/main/radio/index.html', '_blank');}});
-
-//===================================
-$('#b02').click(function(){
-    $('#mainframe').attr('src', 'https://htmlpreview.github.io/?https://github.com/ericjrich/sites/raw/tree/main/cnc/b02/index.html');
-});
-
-$("#b02").mousedown(function(ev){
-      if(ev.which == 3)
-      {window.open('https://htmlpreview.github.io/?https://github.com/ericjrich/sites/raw/tree/main/cnc/b02/index.html', '_blank');}});
-//===================================
-$('#b03').click(function(){
-    $('#mainframe').attr('src', 'https://htmlpreview.github.io/?https://github.com/ericjrich/sites/raw/tree/main/cnc/b03/index.html');
-});
-
-$("#b03").mousedown(function(ev){
-      if(ev.which == 3)
-      {window.open('https://htmlpreview.github.io/?https://github.com/ericjrich/sites/raw/tree/main/cnc/b03/index.html', '_blank');}});
-//===================================
-//===================================
 /*
-$('#b04').click(function(){
-    $('#mainframe').attr('src', '');
-});
-
-$("#b04").mousedown(function(ev){
-      if(ev.which == 3)
-      {window.open('', '_blank');}});
+https://htmlpreview.github.io/?
+https://github.com/ericjrich/sites/main/cnc/index.html
+https://github.com/ericjrich/sites/main/radio/index.html
 */
+see='https://htmlpreview.github.io/?'
+loc='https://github.com/ericjrich/sites/main/'
+b00='b00/index.html'
+b01= see + loc + 'radio/index.html'
+b02='b02/index.html'
+b03='b03/index.html'
+
+$('#b00').click(function(){$('#mainframe').attr('src', b00);});
+$("#b00").mousedown(function(ev){if(ev.which == 3){window.open(b00, '_blank');}});
+
+$('#b01').click(function(){$('#mainframe').attr('src', b01);});
+$("#b01").mousedown(function(ev){if(ev.which == 3){window.open(b01, '_blank');}});
+
+$('#b02').click(function(){$('#mainframe').attr('src', b02);});
+$("#b02").mousedown(function(ev){if(ev.which == 3){window.open(b02, '_blank');}});
+
+$('#b03').click(function(){$('#mainframe').attr('src', b03);});
+$("#b03").mousedown(function(ev){if(ev.which == 3){window.open(b03, '_blank');}});
+
 });
