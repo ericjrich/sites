439c1de847c46a3af081f98f6c9ea27bf8be7127
ejr <64696169+ericjrich@users.noreply.github.com>
2023-01-06 09:41:11 -0500

    Add files via upload

--- cnc/js.js
+++ cnc/js.js
@@ -8,41 +8,41 @@ $('#ejr').hide();
 	$('#pagediv').delay(3000).fadeIn(500);
 //===================================
 //===================================
-//https://htmlpreview.github.io/?https://github.com/ericjrich/sites/tree/main/cnc/index.html
-//https://github.com/ericjrich/sites/blob/main/radio/index.html
+//https://htmlpreview.github.io/?https://github.com/ericjrich/sites/raw/tree/main/cnc/index.html
+//https://github.com/ericjrich/sites/raw/blob/main/radio/index.html
 $('#b00').click(function(){
-    $('#mainframe').attr('src', 'https://htmlpreview.github.io/?https://github.com/ericjrich/sites/tree/main/cnc/b00/index.html');
+    $('#mainframe').attr('src', 'https://htmlpreview.github.io/?https://github.com/ericjrich/sites/raw/tree/main/cnc/b00/index.html');
 });
 
 $("#b00").mousedown(function(ev){
       if(ev.which == 3)
-      {window.open('https://htmlpreview.github.io/?https://github.com/ericjrich/sites/tree/main/cnc/b00/index.html', '_blank');}});
+      {window.open('https://htmlpreview.github.io/?https://github.com/ericjrich/sites/raw/tree/main/cnc/b00/index.html', '_blank');}});
 
 //===================================
 $('#b01').click(function(){
-    $('#mainframe').attr('src', '.https://htmlpreview.github.io/?https://github.com/ericjrich/sites/tree/main/radio/index.html');
+    $('#mainframe').attr('src', '.https://htmlpreview.github.io/?https://github.com/ericjrich/sites/raw/tree/main/radio/index.html');
 });
 
 $("#b01").mousedown(function(ev){
       if(ev.which == 3)
-      {window.open('.https://htmlpreview.github.io/?https://github.com/ericjrich/sites/tree/main/radio/index.html', '_blank');}});
+      {window.open('.https://htmlpreview.github.io/?https://github.com/ericjrich/sites/raw/tree/main/radio/index.html', '_blank');}});
 
 //===================================
 $('#b02').click(function(){
-    $('#mainframe').attr('src', 'https://htmlpreview.github.io/?https://github.com/ericjrich/sites/tree/main/cnc/b02/index.html');
+    $('#mainframe').attr('src', 'https://htmlpreview.github.io/?https://github.com/ericjrich/sites/raw/tree/main/cnc/b02/index.html');
 });
 
 $("#b02").mousedown(function(ev){
       if(ev.which == 3)
-      {window.open('https://htmlpreview.github.io/?https://github.com/ericjrich/sites/tree/main/cnc/b02/index.html', '_blank');}});
+      {window.open('https://htmlpreview.github.io/?https://github.com/ericjrich/sites/raw/tree/main/cnc/b02/index.html', '_blank');}});
 //===================================
 $('#b03').click(function(){
-    $('#mainframe').attr('src', 'https://htmlpreview.github.io/?https://github.com/ericjrich/sites/tree/main/cnc/b03/index.html');
+    $('#mainframe').attr('src', 'https://htmlpreview.github.io/?https://github.com/ericjrich/sites/raw/tree/main/cnc/b03/index.html');
 });
 
 $("#b03").mousedown(function(ev){
       if(ev.which == 3)
-      {window.open('https://htmlpreview.github.io/?https://github.com/ericjrich/sites/tree/main/cnc/b03/index.html', '_blank');}});
+      {window.open('https://htmlpreview.github.io/?https://github.com/ericjrich/sites/raw/tree/main/cnc/b03/index.html', '_blank');}});
 //===================================
 //===================================
 /*
