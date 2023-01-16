983838320c326336c281e8c5d971bb4230e1434b
ejr <64696169+ericjrich@users.noreply.github.com>
2023-01-16 17:44:42 -0500

    Add files via upload

--- apps/js.js
+++ apps/js.js
@@ -1,7 +1,8 @@
 $(function(){
-/*========== hide the start meny widnows ================*/
-$('#tbs0').hide();$('#tbs1').hide();$('#tbs2').hide();$('#tbs3').hide();$('#tbs4').hide();$('#tbs5').hide();$('#tbs6').hide();$('#tbs7').hide();$('#tbs8').hide();$('#tbs9').hide();
 
+
+/*========== hide the start meny widnows ================*/
+$('#tbs0,#tbs1,#tbs2,#tbs3,#tbs4,#tbs5,#tbs6,#tbs7,#tbs8,#tbs9').hide();
 /*========== start menu ==================================================*/
 $('#tbs').click(function(){ $('#tbsmenu').animate({left: '0'}); });
 $("#tbsmenu").mouseleave(function(){$(this).animate({left: '-30%'});});
@@ -9,16 +10,16 @@ $("#tbsmenu").mouseleave(function(){$(this).animate({left: '-30%'});});
 /*======== start menu windows =====================================*/
 $(".tbsx").mouseleave(function(){$(this).fadeOut();});
 
-$('#tbsmenu0').click(function(){ $('#tbs0').fadeToggle();$('#tbs1').hide();$('#tbs2').hide();$('#tbs3').hide();$('#tbs4').hide();$('#tbs5').hide();$('#tbs6').hide();$('#tbs7').hide();$('#tbs8').hide();$('#tbs9').hide();});
-$('#tbsmenu1').click(function(){ $('#tbs1').fadeToggle();$('#tbs0').hide();$('#tbs2').hide();$('#tbs3').hide();$('#tbs4').hide();$('#tbs5').hide();$('#tbs6').hide();$('#tbs7').hide();$('#tbs8').hide();$('#tbs9').hide();});
-$('#tbsmenu2').click(function(){ $('#tbs2').fadeToggle();$('#tbs0').hide();$('#tbs1').hide();$('#tbs3').hide();$('#tbs4').hide();$('#tbs5').hide();$('#tbs6').hide();$('#tbs7').hide();$('#tbs8').hide();$('#tbs9').hide();});
-$('#tbsmenu3').click(function(){ $('#tbs3').fadeToggle();$('#tbs0').hide();$('#tbs1').hide();$('#tbs2').hide();$('#tbs4').hide();$('#tbs5').hide();$('#tbs6').hide();$('#tbs7').hide();$('#tbs8').hide();$('#tbs9').hide();});
-$('#tbsmenu4').click(function(){ $('#tbs4').fadeToggle();$('#tbs0').hide();$('#tbs1').hide();$('#tbs2').hide();$('#tbs3').hide();$('#tbs5').hide();$('#tbs6').hide();$('#tbs7').hide();$('#tbs8').hide();$('#tbs9').hide();});
-$('#tbsmenu5').click(function(){ $('#tbs5').fadeToggle();$('#tbs0').hide();$('#tbs1').hide();$('#tbs2').hide();$('#tbs3').hide();$('#tbs4').hide();$('#tbs6').hide();$('#tbs7').hide();$('#tbs8').hide();$('#tbs9').hide();});
-$('#tbsmenu6').click(function(){ $('#tbs6').fadeToggle();$('#tbs0').hide();$('#tbs1').hide();$('#tbs2').hide();$('#tbs3').hide();$('#tbs4').hide();$('#tbs5').hide();$('#tbs7').hide();$('#tbs8').hide();$('#tbs9').hide();});
-$('#tbsmenu7').click(function(){ $('#tbs7').fadeToggle();$('#tbs0').hide();$('#tbs1').hide();$('#tbs2').hide();$('#tbs3').hide();$('#tbs4').hide();$('#tbs5').hide();$('#tbs6').hide();$('#tbs8').hide();$('#tbs9').hide();});
-$('#tbsmenu8').click(function(){ $('#tbs8').fadeToggle();$('#tbs0').hide();$('#tbs1').hide();$('#tbs2').hide();$('#tbs3').hide();$('#tbs4').hide();$('#tbs5').hide();$('#tbs6').hide();$('#tbs7').hide();$('#tbs9').hide();});
-$('#tbsmenu9').click(function(){ $('#tbs9').fadeToggle();$('#tbs0').hide();$('#tbs1').hide();$('#tbs2').hide();$('#tbs3').hide();$('#tbs4').hide();$('#tbs5').hide();$('#tbs6').hide();$('#tbs7').hide();$('#tbs8').hide();});
+$('#tbsmenu0').click(function(){ $('#tbs0').fadeToggle();$('#tbs1,#tbs2,#tbs3,#tbs4,#tbs5,#tbs6,#tbs7,#tbs8,#tbs9').hide();});
+$('#tbsmenu1').click(function(){ $('#tbs1').fadeToggle();$('#tbs0,#tbs2,#tbs3,#tbs4,#tbs5,#tbs6,#tbs7,#tbs8,#tbs9').hide();});
+$('#tbsmenu2').click(function(){ $('#tbs2').fadeToggle();$('#tbs0,#tbs1,#tbs3,#tbs4,#tbs5,#tbs6,#tbs7,#tbs8,#tbs9').hide();});
+$('#tbsmenu3').click(function(){ $('#tbs3').fadeToggle();$('#tbs0,#tbs1,#tbs2,#tbs4,#tbs5,#tbs6,#tbs7,#tbs8,#tbs9').hide();});
+$('#tbsmenu4').click(function(){ $('#tbs4').fadeToggle();$('#tbs0,#tbs1,#tbs2,#tbs3,#tbs5,#tbs6,#tbs7,#tbs8,#tbs9').hide();});
+$('#tbsmenu5').click(function(){ $('#tbs5').fadeToggle();$('#tbs0,#tbs1,#tbs2,#tbs3,#tbs4,#tbs6,#tbs7,#tbs8,#tbs9').hide();});
+$('#tbsmenu6').click(function(){ $('#tbs6').fadeToggle();$('#tbs0,#tbs1,#tbs2,#tbs3,#tbs4,#tbs5,#tbs7,#tbs8,#tbs9').hide();});
+$('#tbsmenu7').click(function(){ $('#tbs7').fadeToggle();$('#tbs0,#tbs1,#tbs2,#tbs3,#tbs4,#tbs5,#tbs6,#tbs8,#tbs9').hide();});
+$('#tbsmenu8').click(function(){ $('#tbs8').fadeToggle();$('#tbs0,#tbs1,#tbs2,#tbs3,#tbs4,#tbs5,#tbs6,#tbs7,#tbs9').hide();});
+$('#tbsmenu9').click(function(){ $('#tbs9').fadeToggle();$('#tbs0,#tbs1,#tbs2,#tbs3,#tbs4,#tbs5,#tbs6,#tbs7,#tbs8').hide();});
 
 $(document).mouseup(function(e){var container = $("#tbs0");if(!container.is(e.target) && container.has(e.target).length === 0){container.fadeOut();}});
 $(document).mouseup(function(e){var container = $("#tbs1");if(!container.is(e.target) && container.has(e.target).length === 0){container.fadeOut();}});
