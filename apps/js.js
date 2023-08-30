8daf6c9913d9b2955fd665bb0e345801d92027e1
ejr <64696169+ericjrich@users.noreply.github.com>
2023-08-30 12:27:38 -0400

    Add files via upload

--- apps/js.js
+++ apps/js.js
@@ -1,14 +1,13 @@
 $(function(){
-/*XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX*/
-	/*#### HIDE STUFF #####*/
+/*XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX*/
+/*#### HIDE STUFF #####*/
 	$('#d3,.d4,#d5').hide();
-
-	/*#### START MENU #####*/
+/*#### START MENU #####*/
 	$('#d2-1').click(function(){ $('#d3').slideDown();});
 	$("#d3").mouseleave(function(){$(this).slideUp();});
-	/**/
+/**/
 	$(".d4").mouseleave(function(){$(this).fadeOut();});
-	/**/
+/**/
 	$('#d3-8').click(function(){ $('.d4').hide();$('#d4-8').fadeToggle(500);});
 	$('#d3-7').click(function(){ $('.d4').hide();$('#d4-7').fadeToggle(500);});
 	$('#d3-6').click(function(){ $('.d4').hide();$('#d4-6').fadeToggle(500);});
@@ -17,19 +16,23 @@ $(function(){
 	$('#d3-3').click(function(){ $('.d4').hide();$('#d4-3').fadeToggle(500);});
 	$('#d3-2').click(function(){ $('.d4').hide();$('#d4-2').fadeToggle(500);});
 	$('#d3-1').click(function(){ $('.d4').hide();$('#d4-1').fadeToggle(500);});
-
-	/*#### WEATHER BOX ####*/
+/*#### WEATHER BOX ####*/
 	$('#d2-6').click(function(){ $('#d5').fadeToggle('1000');});
-
-	/*#### DATE & TIME ####*/
+/*#### DATE & TIME ####*/
 	function ztime() {
 		var d = new Date(),
-			minutes = d.getMinutes().toString().length == 1 ? '0'+d.getMinutes() : d.getMinutes(),
-		  hours = d.getHours().toString().length == 1 ? '0'+d.getHours() : d.getHours(),
-		  months = ['01','02','03','04','05','06','07','08','09','10','11','12'],
-		  days = ['Sun','Mon','Tue','Wed','Thu','Fri','Sat'];
-  	return d.getFullYear()+'/'+months[d.getMonth()]+'/'+d.getDate()+' '+hours+':'+minutes+' '+days[d.getDay()];
+		minutes = d.getMinutes().toString().length == 1 ? '0'+d.getMinutes() : d.getMinutes(),
+		hours = d.getHours().toString().length == 1 ? '0'+d.getHours() : d.getHours(),
+		months = ['01','02','03','04','05','06','07','08','09','10','11','12'],
+		days = ['Sun','Mon','Tue','Wed','Thu','Fri','Sat'];
+		// Update the title attribute of the div with the formatted date
+			$('#d2-4').attr('title', d.getFullYear()+'/'+months[d.getMonth()]+'/'+d.getDate());
+		// Update the #time div with the formatted time
+			$('#time').html(hours + ':' + minutes);
 	}
-	document.getElementById('time').innerHTML= ztime();
-/*XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX*/
+	//---- call it & set intervals-----
+		ztime();
+		setInterval(ztime, 1000);
+
+/*XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX*/
 });
